bdbb3f5947499288fc8468ac60feb43fa10fda0a
asumandang <32838966+asumandang@users.noreply.github.com>
2018-12-26 13:34:18 +0800

    Update simpleBanking.cpp

--- simpleBanking.cpp
+++ simpleBanking.cpp
@@ -3,13 +3,13 @@ This is a simple banking system implemented by Torres, Sumandang, and Alvaro.
 
 The simple banking system can:
 
-1. create new account
+1. create new account;
 	- savings
 	- current
-2. read account information
-3. update account information
-4. delete account
-5. withdraw/deposit/transfer money from/to accounts
+2. read account information;
+3. update account information;
+4. delete account;
+5. withdraw/deposit/transfer money from/to accounts, and;
 6. increase the deposited money base on the interest rate depending on the account type. (Savings or Current)
 
 */
